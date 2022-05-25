8d2a4fee71f67282260947434582be5b748cdaf5
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-05-25 16:43:47 -0300

    chore: add bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,26 +2,37 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { PrismaService } from 'src/database/prisma.service';
 import { AuthDTO } from './dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signUp(dto: AuthDTO) {
+    const { password } = dto;
+    const hashedPassword = await this.hashPassword(password);
+    console.log(hashedPassword);
+
     try {
       const user = await this.prisma.user.create({
         data: {
           email: dto.email,
-          password: dto.password,
+          password: hashedPassword,
         },
       });
+      delete user.password;
+      return user;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
           throw new ForbiddenException('Credentials taken');
         }
-      }
-      throw new error();
+      }      
     }
+  }
+
+  private async hashPassword(password: string) {
+    const salt = 10;
+    return bcrypt.hash(password, salt);
   }
 }
