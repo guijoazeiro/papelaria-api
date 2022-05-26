059d68498e7b902b870fa5bcae7fddd75dc2a772
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-05-26 16:18:43 -0300

    chore: add jwt sign method

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import {JwtModule} from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService, PrismaService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,26 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { PrismaService } from 'src/database/prisma.service';
 import { AuthDTO } from './dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signUp(dto: AuthDTO) {
     const { password } = dto;
-    const hashedPassword = await this.hashPassword(password);
+    const hashedPassword =
+      await this.hashPassword(password);
 
     try {
       const user = await this.prisma.user.create({
@@ -19,12 +29,17 @@ export class AuthService {
           password: hashedPassword,
         },
       });
-      delete user.password;
-      return user;
+
+      return this.signToken(user.id, user.email);
     } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
         if (error.code === 'P2002') {
-          throw new ForbiddenException('Credentials taken');
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
         }
       }
     }
@@ -32,25 +47,47 @@ export class AuthService {
 
   async signIn(dto: AuthDTO) {
     const { email, password } = dto;
-    const user = await this.prisma.user.findUnique({
-      where: { email },
-    });
+    const user =
+      await this.prisma.user.findUnique({
+        where: { email },
+      });
 
     if (!user) {
-      throw new ForbiddenException('Credentials not found');
+      throw new ForbiddenException(
+        'Credentials not found',
+      );
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      user.password,
+    );
     if (!isPasswordValid) {
-      throw new ForbiddenException('Credentials not found');
+      throw new ForbiddenException(
+        'Credentials not found',
+      );
     }
-
-    delete user.password;
-    return user;
+    return this.signToken(user.id, user.email);
   }
 
-  private async hashPassword(password: string) {
+  private async hashPassword(
+    password: string,
+  ): Promise<string> {
     const salt = 10;
     return bcrypt.hash(password, salt);
+  }
+
+  signToken(userId: string, email: string) {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    return this.jwt.signAsync(payload, {
+      expiresIn: '1d',
+      secret: secret,
+    });
   }
 }
