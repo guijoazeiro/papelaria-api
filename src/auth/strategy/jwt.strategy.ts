1d31c5ed117a73c24223b8fe106ae6b7eed42521
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-05-26 17:47:31 -0300

    chore: add jwtStrategy validate method

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -5,16 +5,34 @@ import {
   ExtractJwt,
   Strategy,
 } from 'passport-jwt';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(
   Strategy,
 ) {
-  constructor(config: ConfigService) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
     super({
       jwtFromRequest:
         ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+
+  async validate(payload: {
+    sub: string;
+    email: string;
+  }) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          id: payload.sub,
+        },
+      });
+    delete user.password;
+    return user;
   }
 }
