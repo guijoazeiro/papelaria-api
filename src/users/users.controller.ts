f7f4d3705dd85dc6e59e3261c160d008a30ac5dc
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-05-27 15:54:29 -0300

    chore: add users module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(
+    private readonly usersService: UsersService,
+  ) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  async getMe(@Req() req: Request) {
+    return req.user;
+  }
+}
