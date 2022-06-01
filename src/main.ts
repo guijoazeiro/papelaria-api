4b7360908be1f455f72dd34148dbe795e76e18ed
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-06-01 17:02:00 -0300

    chore: add prisma exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import {
+  HttpAdapterHost,
+  NestFactory,
+} from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true }),
+  );
+
+  const { httpAdapter } = app.get(
+    HttpAdapterHost,
+  );
+  app.useGlobalFilters(
+    new PrismaClientExceptionFilter(httpAdapter),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
