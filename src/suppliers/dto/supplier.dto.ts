34e82b01b7a95b3d08fd25bba4a521d585d5801a
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-06-14 17:19:04 -0300

    chore: add OpenAPI decorators to Supllier Controller

--- src/suppliers/dto/supplier.dto.ts
+++ src/suppliers/dto/supplier.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
-  IsString,
+  IsString
 } from 'class-validator';
 
 export class SupplierDTO {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   cnpj: string;
--- src/suppliers/suppliers.controller.ts
+++ src/suppliers/suppliers.controller.ts
@@ -8,17 +8,36 @@ import {
   Put,
   UseGuards
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiResponse,
+  ApiTags
+} from '@nestjs/swagger';
 import { JwtGuard } from 'src/auth/guard';
 import { RolesGuard } from 'src/auth/guard/role.guard';
 import { SupplierDTO } from './dto';
 import { SuppliersService } from './suppliers.service';
 
+@ApiTags('suppliers')
 @Controller('suppliers')
 export class SuppliersController {
   constructor(
     private readonly suppliersService: SuppliersService,
   ) {}
 
+  @ApiBody({
+    type: SupplierDTO,
+  })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 201,
+    description: 'Supplier created successfully',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
   @UseGuards(JwtGuard, RolesGuard)
   @Post()
   async crateSupplier(
@@ -29,11 +48,21 @@ export class SuppliersController {
     );
   }
 
+  @ApiResponse({
+    status: 200,
+    description:
+      'Suppliers retrieved successfully',
+  })
   @Get()
   async getSuppliers() {
     return await this.suppliersService.getSuppliers();
   }
 
+  @ApiResponse({
+    status: 200,
+    description:
+      'Supplier retrieved successfully',
+  })
   @Get('/:cnpj')
   async getSupplier(@Param('cnpj') cnpj: string) {
     return await this.suppliersService.getSupplier(
@@ -41,6 +70,18 @@ export class SuppliersController {
     );
   }
 
+  @ApiBody({
+    type: SupplierDTO,
+  })
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 201,
+    description: 'Supplier updated successfully',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
   @UseGuards(JwtGuard, RolesGuard)
   @Put('/:cnpj')
   async updateSupplier(
@@ -53,6 +94,15 @@ export class SuppliersController {
     );
   }
 
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 201,
+    description: 'Supplier deleted successfully',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
   @UseGuards(JwtGuard, RolesGuard)
   @Delete('/:cnpj')
   async deleteSupplier(
