0ef5a8221a8a0cf7c68a9ad6820ae143fe32d0e1
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-06-14 15:43:59 -0300

    chore: add OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import {
   HttpAdapterHost,
-  NestFactory,
+  NestFactory
 } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerModule
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 
@@ -18,6 +22,20 @@ async function bootstrap() {
   app.useGlobalFilters(
     new PrismaClientExceptionFilter(httpAdapter),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Eccomerce API')
+    .setDescription(
+      'Eccomcerce API using Nestjs + Prisma',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(
+    app,
+    config,
+  );
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
